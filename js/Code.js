bb232ecc12dce3a514242506d4ef908239801c92
LAKSH-2868 <76722236+LAKSH-2868@users.noreply.github.com>
2021-06-12 17:59:26 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,34 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+const accessCode4 = "CONSTANT";
+
+function clues() {
+    
+fill("white")
+textSize(15)
+text("R E V B A I L A", 100,50)
+fill("lightblue")
+text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+textSize(15)
+text("N T F O U I N C ", 700,160)
+fill("lightblue")
+text("Hint:Designed to peform particular task !!", 700,180)
+
+fill("white")
+textSize(15)
+text("A A A B T D E S", 100,250)
+fill("lightblue")
+text("Hint: Stores all information !!", 100,270)
+
+fill("white")
+textSize(15)
+text(" T T C N N A S O", 700,350)
+fill("lightblue")
+text("Hint: Always remain same!!", 700,370)
+    
+
+}
---
+++ js/Security.js
@@ -0,0 +1,81 @@
+class Security {
+
+    constructor(){
+
+        this.access1 = createInput("Code1")
+        this.access1.position(100,90);
+        this.access1.style('background', 'white');  
+
+        this.button1 = createButton('Check');
+        this.button1.position(100,120);
+        this.button1.style('background', 'lightgrey');    
+
+        this.access2 = createInput("Code1")
+        this.access2.position(700,190);
+        this.access2.style('background', 'white');  
+
+        this.button2 = createButton('Check');
+        this.button2.position(700,220);
+        this.button2.style('background', 'lightgrey');
+
+        this.access3 = createInput("Code3")
+        this.access3.position(100,280)
+        this.access3.style('background' , 'white ')
+
+        this.button3 = createButton('Check')
+        this.button3.position(100,310);
+        this.button3.style('background' , 'lightgrey');
+
+        this.access4 = createInput("Code4")
+        this.access4.position(700,380)
+        this.access4.style('background' , 'white ')
+
+        this.button4 = createButton('Check')
+        this.button4.position(700,410);
+        this.button4.style('background' , 'lightgrey');
+
+
+//add code for creating and positioning the third input box and button
+        
+    }
+
+    display(){
+
+        this.button1.mousePressed(() => {
+            if(system.authenticate(accessCode1,this.access1.value())){
+                this.button1.hide();
+                this.access1.hide();
+                score++;
+            }
+        });
+
+        this.button2.mousePressed(() => {
+            if(system.authenticate(accessCode2,this.access2.value())){
+                this.button2.hide();
+                this.access2.hide();
+                score++;
+            }
+        });
+
+        this.button3.mousePressed(()=>{
+            if(system.authenticate(accessCode3,this.access3.value())){
+                this.button3.hide();
+                this.access3.hide();
+                score++;
+            }
+        });
+
+        this.button4.mousePressed(()=>{
+            if(system.authenticate(accessCode4,this.access4.value())){
+                this.button4.hide();
+                this.access4.hide();
+                score++;
+            }
+        });
+
+
+//add code for what happens when the third button is pressed
+        
+
+    }
+}
